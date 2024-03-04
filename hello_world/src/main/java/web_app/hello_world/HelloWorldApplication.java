bc54b95d4f7681e7984507831902a98024989005
epicestudar <viniciusftza@gmail.com>
2024-03-04 14:50:18 -0300

    hello, world!

---
+++ hello_world/src/main/java/web_app/hello_world/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package web_app.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
