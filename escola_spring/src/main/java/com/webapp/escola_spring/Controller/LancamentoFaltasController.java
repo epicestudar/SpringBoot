76accbc90f24c2bb529332ec7cf7c5a97fad9a9f
epicestudar <viniciusftza@gmail.com>
2024-04-28 13:58:50 -0300

    ajustando controller de faltas e melhorando o frontend

--- escola_spring/src/main/java/com/webapp/escola_spring/Controller/LancamentoFaltasController.java
+++ escola_spring/src/main/java/com/webapp/escola_spring/Controller/LancamentoFaltasController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +16,9 @@ import com.webapp.escola_spring.Model.Aluno;
 import com.webapp.escola_spring.Model.LancarFaltas;
 import com.webapp.escola_spring.Repository.AlunoRepository;
 import com.webapp.escola_spring.Repository.LancarFaltasRepository;
-
 import jakarta.servlet.http.HttpSession;
 
+@Controller
 public class LancamentoFaltasController {
     @Autowired
     private AlunoRepository alunoRepository;
